4dad46111c6ff39ada34d46ba8c1d048bf11c727
yzhuangintcd <127947054+yzhuangintcd@users.noreply.github.com>
2025-02-22 20:01:19 +0000

    Replace Firebase Config variables in .env

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -13,13 +13,13 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDgVD-F43QdrsHLCrYt9_sL-kuR4zWynj4",
-  authDomain: "hackirelandgroup37-e6489.firebaseapp.com",
-  projectId: "hackirelandgroup37-e6489",
-  storageBucket: "hackirelandgroup37-e6489.firebasestorage.app",
-  messagingSenderId: "74112220516",
-  appId: "1:74112220516:web:3f4a3a68e7c24c92cd8e71",
-  measurementId: "G-K4VTXGHZ6Y",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
