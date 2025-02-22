00ada41bdd3e9c565ff1d8374389f4c1040e7c07
yzhuangintcd <127947054+yzhuangintcd@users.noreply.github.com>
2025-02-22 19:55:47 +0000

    Set up Gtihub Pages

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Group_37_HackTrinity_Frontend/', // Your repo name here
   plugins: [react()],
 })
